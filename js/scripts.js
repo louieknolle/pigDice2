9c7ab116590bfc76bf036d920e657f4c0bc28833
Louis Knolle <knollelw@gmail.com>
2022-05-01 19:37:40 -0400

    create ui html and attempt more logic
    Co-authored by: Oscar Gonzalez <ogon318@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,19 +16,34 @@ Player.prototype.switchCurrentPlayer = function() {
 
 
 // alt object
-let player1 = '';
-let player2 = '';
-const throwDice = function() {
-  return Math.floor(Math.random() * 6) + 1;
-}
+// let player1 = '';
+// let player2 = '';
+// const throwDice = function() {
+//   return Math.floor(Math.random() * 6) + 1;
+// }
 
-function Player(turn) {
-  this.roll = 0;
-  this.currentScore = 0;
-  this.totalScore = 0;
-  this.turn = turn;
-  this.playerName;
-}
+// function Player(turn) {
+//   this.roll = 0;
+//   this.currentScore = 0;
+//   this.totalScore = 0;
+//   this.turn = turn;
+//   this.playerName;
+// }
+
+// //player1or2.roll = throwdice();
+
+// Player.prototype.rollOne = function() {
+//   if (this.roll === 1) {
+//     this.currentScore = 0;
+//     // Change turn?
+//   } else {
+//     this.currentScore += this.roll;
+//   }
+// }
+
+// Player.prototype.changeTurn = function() {
+
+// }
 
 
 // UI Logic 
