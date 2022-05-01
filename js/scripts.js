aaa49655054615049801d08c46f4060d7fd7aa8d
Louis Knolle <knollelw@gmail.com>
2022-05-01 17:03:22 -0400

    try different object and add co-author
    Co-authored by: Oscar Gonzalez <ogon318@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,31 @@ function Player(tonyPlayer, mikePlayer) {
   this.currentPlayer;
   this.gameBegin = false; 
 }
-Player.prototype.switchCurrentPlayer = () {
-  if ()
+Player.prototype.switchCurrentPlayer = function() {
+  if () {
+
+  }
+}
+
+
+// alt object
+let player1 = '';
+let player2 = '';
+const throwDice = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+
+// UI Logic 
 $(document).ready(function() {
   
 });
+
